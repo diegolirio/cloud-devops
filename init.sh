8d1bbb592e40eec7ca88f2b61d277802ed803194
Diego Lirio D Pereira <diego.pereira@accesstage.com.br>
2018-01-23 18:42:05 -0200

    Install docker-compose sh

--- init.sh
+++ init.sh
@@ -1,15 +1,13 @@
-apt-get update 
+echo ".............................. *************************** .............................."
+echo ".............................. *************************** .............................."
+echo "LETS INSTALL DOCKER CE"
+echo ".............................. *************************** .............................."
+echo ".............................. *************************** .............................."
+sh ./install-docker.sh
 
-apt-get install apt-transport-https ca-certificates curl software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-apt-key fingerprint 0EBFCD88
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt-get update
-
-apt-get install docker-ce
-
-apt-cache madison docker-ce
+echo ".............................. *************************** .............................."
+echo ".............................. *************************** .............................."
+echo "LETS INSTALL DOCKER CE"
+echo ".............................. *************************** .............................."
+echo ".............................. *************************** .............................."
+sh ./install-docker-compose.sh
