a55e0a3bc8cf27fe9cfa20a0cb1f8b9b6ceb6cba
Diego Lirio D Pereira <diego.pereira@accesstage.com.br>
2018-01-26 16:00:08 -0200

    Build Env dev/hom/prod

--- registry.sh
+++ registry.sh
@@ -1,2 +1,20 @@
-docker-compose -f docker-compose-registry.yml down
-docker-compose -f docker-compose-registry.yml up -d
+if ["$1" = "" ]; then
+   echo "Param name required (dev or hom or prod)"
+   exit 1
+fi
+
+env=$1
+
+if [ $env = "prod" ]; then
+    docker-compose -f docker-compose-registry-prod.yml down
+    docker-compose -f docker-compose-registry-prod.yml up -d
+elif [ $env = "hom" ]; then
+    docker-compose -f docker-compose-registry-hom.yml down
+    docker-compose -f docker-compose-registry-hom.yml up -d    
+elif [ $env = "dev" ]; then
+    docker-compose -f docker-compose-registry-dev.yml down
+    docker-compose -f docker-compose-registry-dev.yml up -d       
+else
+    echo "Env Not Implemented!!!"
+    echo "Registry not created"
+fi
