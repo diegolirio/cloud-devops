f12aa8e583cfa77e70b33fb6ba134bb4cab4fd9b
Diego Lirio <diegolirio.dl@gmail.com>
2018-01-29 22:14:26 -0200

    Problems with certs, fallback

--- registry/registry.sh
+++ registry/registry.sh
@@ -11,10 +11,10 @@ if [ $env = "prod" ]; then
     docker-compose -f docker-compose-registry-prod.yml down
     docker-compose -f docker-compose-registry-prod.yml up -d
 elif [ $env = "hom" ]; then
-    #docker-compose -f docker-compose-registry-hom.yml down
-    #docker-compose -f docker-compose-registry-hom.yml up -d  
-    docker rm -f registryhom  
-    docker run -d -p 5000:5000 -v $dir/certs:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.cert -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key --restart=always --name registryhom registry:2 
+    docker-compose -f docker-compose-registry-hom.yml down
+    docker-compose -f docker-compose-registry-hom.yml up -d  
+    #docker rm -f registryhom  
+    #docker run -d -p 5000:5000 -v $dir/certs:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.cert -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key --restart=always --name registryhom registry:2 
 elif [ $env = "dev" ]; then
     docker-compose -f docker-compose-registry-dev.yml down
     docker-compose -f docker-compose-registry-dev.yml up -d       
