d8db5ad33ef4ea49efa8ecbc4f284d920864c7a8
Diego Lirio D Pereira <diego.pereira@accesstage.com.br>
2018-02-14 12:08:27 -0200

    refactor

--- registry/copy-certs-client.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d /etc/docker/certs.d/registry.phoenix4go.com ]
-then
-    mkdir -p /etc/docker/certs.d/registry.phoenix4go.com 
-fi
-rm -rf /etc/docker/certs.d/registry.phoenix4go.com/ca.crt
-cp certs_prod/domain.cert /etc/docker/certs.d/registry.phoenix4go.com/ca.crt
